a80d9a99d0ba6f98038f3b493749596bd6d059b0
Sultan Aloufi <156688701+sumaf@users.noreply.github.com>
2026-07-03 10:32:28 +0300

    Got rid of the useless prefix func in resp. Each type should build its own byte array

--- app/resp.go
+++ app/resp.go
@@ -20,23 +20,19 @@ const (
 	respnil = '_'
 )
 
-func AppendPrefix(s []byte, p byte, n int64) []byte {
-	s = append(s, p)
-	s = strconv.AppendInt(s, n, 10)
-	return append(s, '\r', '\n')
-}
-
 func GetSimpleString(simple string) []byte {
-	s := append([]byte("+"), simple...)
+	s := append([]byte{respstr}, simple...)
 	return append(s, '\r', '\n')
 }
 
 func GetBulkString(bulk string) []byte {
-	s := AppendPrefix([]byte{}, '$', int64(len(bulk)))
+	s := strconv.AppendInt([]byte{respblk}, int64(len(bulk)), 10)
+	s = append(s, '\r', '\n')
 	s = append(s, bulk...)
 	return append(s, '\r', '\n')
 }
 
 func GetNullBulkString() []byte {
-	return AppendPrefix([]byte{}, '$', -1)
+	s := strconv.AppendInt([]byte{respblk}, -1, 10)
+	return append(s, '\r', '\n')
 }
